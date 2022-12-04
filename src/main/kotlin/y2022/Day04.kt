87456f23d54bd8e82e2ac38c8eae7aeae7273c3e
aottolini <aottolini@agro.uba.ar>
2022-12-04 14:21:31 -0300

    added day 4 2022

---
+++ src/main/kotlin/y2022/Day04.kt
@@ -0,0 +1,27 @@
+package y2022
+
+import java.io.File
+
+typealias ElfSections = IntRange
+
+typealias ElfSectionsPair = Pair<ElfSections, ElfSections>
+
+fun elfSectionsPairs(file: File): List<ElfSectionsPair> =
+    file.readLines()
+        .flatMap { it.split(",") }
+        .map { it.split("-") }
+        .map { it.first().toInt()..it.last().toInt() }
+        .chunked(2)
+        .map { it.first() to it.last() }
+
+fun fullOverlap(pair: ElfSectionsPair): Boolean =
+    pair.first.all { pair.second.contains(it) } || pair.second.all { pair.first.contains(it) }
+
+fun overlap(pair: ElfSectionsPair): Boolean =
+    pair.first.any { pair.second.contains(it) }
+
+fun fullOverlapCount(elfSectionsPairs: List<ElfSectionsPair>): Int =
+    elfSectionsPairs.count(::fullOverlap)
+
+fun overlapCount(elfSectionsPairs: List<ElfSectionsPair>): Int =
+    elfSectionsPairs.count(::overlap)
---
+++ src/test/kotlin/y2022/Day04.kt
@@ -0,0 +1,23 @@
+package y2022
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.File
+
+internal class Day04 {
+
+    private val input = File("src/test/resources/y2022/Day04.txt")
+
+    @Test
+    fun part1() {
+        val elfSectionsPairs = elfSectionsPairs(input)
+        assertEquals(424, fullOverlapCount(elfSectionsPairs))
+    }
+
+    @Test
+    fun part2() {
+        val elfSectionsPairs = elfSectionsPairs(input)
+        assertEquals(804, overlapCount(elfSectionsPairs))
+    }
+
+}
