b925cf75e27a076a8ec664c8b08154fe86979196
aottolini <aottolini@agro.uba.ar>
2022-12-07 00:49:52 -0300

    nitpick

--- src/main/kotlin/y2022/Day06.kt
+++ src/main/kotlin/y2022/Day06.kt
@@ -8,7 +8,7 @@ enum class Marker(val size: Int) { StartOfPacket(4), StartOfMessage(14) }
 
 fun signal(file: File): Signal = file.readText().asSequence()
 
-fun countProcessedSignal(signal: Signal, marker: Marker): Int {
+fun countProcessedSignal(signal: Signal, marker: Marker): Int? {
     var count = 0
     for (signalFrame in signal.windowed(marker.size)) {
         if (signalFrame.distinct().count() == marker.size) {
@@ -16,5 +16,5 @@ fun countProcessedSignal(signal: Signal, marker: Marker): Int {
         }
         count++
     }
-    return count
+    return null
 }
