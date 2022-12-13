c5f779ba3e0e11345e96b9a155d4c2c298ea4bd8
aottolini <aottolini@agro.uba.ar>
2022-12-13 02:48:06 -0300

    format

--- src/main/kotlin/y2022/Day10.kt
+++ src/main/kotlin/y2022/Day10.kt
@@ -11,20 +11,20 @@ fun signalStrength(instructions: List<Instruction>): Int {
     var x = 1
     var strength = 0
     var cycle = 1
+    val observedCycles = setOf(20, 60, 100, 140, 180, 220)
     for (instruction in instructions) {
         when (instruction) {
             is Instruction.AddX -> {
                 cycle++
-                if (cycle in setOf(20, 60, 100, 140, 180, 220)) {
-                    strength += cycle * x
-                }
+                if (cycle in observedCycles) strength += cycle * x
                 x += instruction.value
+                cycle++
+                if (cycle in observedCycles) strength += cycle * x
             }
-            is Instruction.Noop -> {}
-        }
-        cycle++
-        if (cycle in setOf(20, 60, 100, 140, 180, 220)) {
-            strength += cycle * x
+            is Instruction.Noop -> {
+                cycle++
+                if (cycle in observedCycles) strength += cycle * x
+            }
         }
     }
     return strength
