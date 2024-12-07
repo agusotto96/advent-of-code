97faed1e8f15403d9799d5a175fdebc5f59cfdd7
aottolini <aottolini@agro.uba.ar>
2024-12-07 02:14:39 -0300

    day7 scaffolding

---
+++ kotlin/src/test/kotlin/y2019/Day07.kt
@@ -0,0 +1,22 @@
+package y2019
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.File
+
+internal class Day07 {
+
+    private val input = File("../inputs/201907.txt")
+
+    @Test
+    fun part1() {
+        // val intcodes = intcodes(input)
+        // assertEquals(10987514, run(intcodes.toMutableList(), noun = null, verb = null, input = 1))
+    }
+
+    @Test
+    fun part2() {
+
+    }
+
+}
