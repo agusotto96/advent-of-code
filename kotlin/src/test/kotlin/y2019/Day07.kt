a2edd3a27b602f4281cb367636ae536ce9feb458
aottolini <aottolini@agro.uba.ar>
2025-09-09 23:16:17 -0300

    i dont remember this but whatever

--- kotlin/src/test/kotlin/y2019/Day07.kt
+++ kotlin/src/test/kotlin/y2019/Day07.kt
@@ -24,6 +24,22 @@ internal class Day07 {
         assertEquals(38834, max)
     }
 
+    @Test
+    fun part2() {
+        val intcodes = intcodes(input)
+        var max = 0
+        for (permutation in permutations(listOf(5, 6, 7, 8, 9))) {
+            var input = 0
+            for (phase in permutation) {
+                input = run(intcodes.toMutableList(), null, null, phase, input)
+            }
+            if (input > max) {
+                max = input
+            }
+        }
+        assertEquals(38834, max)
+    }
+
     fun permutations(elements: List<Int>): Sequence<List<Int>> = sequence {
         if (elements.size == 1) {
             yield(elements)
